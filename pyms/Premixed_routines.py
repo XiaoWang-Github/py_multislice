a78a42785bf9a6cea964635fcd0546e9709d7937
Xiao Wang (xf9) <xf9@lucky.ornl.gov>
2021-09-25 15:13:38 -0400

    change output type in CBED fnction to np.complex128

--- pyms/Premixed_routines.py
+++ pyms/Premixed_routines.py
@@ -1315,7 +1315,7 @@ def CBED(
 
     t_ = np.asarray(ensure_array(thicknesses))
 
-    output = np.zeros((t_.shape[0], *np.asarray(gridshape)))
+    output = np.zeros((t_.shape[0], *np.asarray(gridshape)),dtype=np.complex128)
 
     if nslices is None:
         # Convert thicknesses into number of slices for multislice
@@ -1492,7 +1492,7 @@ def my_testCBED(
 
     t_ = np.asarray(ensure_array(thicknesses))
 
-    output = np.zeros((t_.shape[0], *np.asarray(gridshape)))
+    output = np.zeros((t_.shape[0], *np.asarray(gridshape)),dtype=np.complex128)
 
     if nslices is None:
         # Convert thicknesses into number of slices for multislice
@@ -1542,7 +1542,7 @@ def my_testCBED(
                 seed=seed,
             )
 
-            output[it] += probe
+            output[it] = probe
 
     # Divide output by # of pixels to compensate for Fourier transform
     return output, P, T, probe_location
